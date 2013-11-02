148fd47027e097cc676ce43b6f1d8edc00688c46
Keith Trnka <trnka.dev@gmail.com>
2013-11-02 14:27:16 -0700

    Adding initial version.

--- NotiponProject/Notipon/src/main/java/com/notipon/MainService.java
+++ NotiponProject/Notipon/src/main/java/com/notipon/MainService.java
@@ -1,7 +1,49 @@
 package com.notipon;
 
+import android.app.Service;
+import android.os.Handler;
+import android.os.IBinder;
+import android.content.Intent;
+import android.util.Log;
+
 /**
  * Created by keith on 11/2/13.
  */
-public class MainService {
+public class MainService extends Service  {
+    public static final String TAG = "MainService";
+    public static final String BROADCAST_ACTION = "com.notipon.MainService.broadcast";
+    private Handler handler = new Handler();
+    private Intent intent;
+    private static final int DELAY_MS = 5000;
+    private Runnable updateRunnable;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        updateRunnable = new Runnable() {
+            public void run() {
+                // call the actual update function
+
+                handler.postDelayed(this, DELAY_MS);
+            }
+        };
+        intent = new Intent(BROADCAST_ACTION);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (updateRunnable == null) {
+            handler.removeCallbacks(updateRunnable);
+            handler.postDelayed(updateRunnable, DELAY_MS);
+        }
+        else {
+            Log.e(TAG, "updateRunnable is null");
+        }
+
+        return START_STICKY;
+    }
+
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
